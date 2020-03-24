0ee0037f156f401e791251c95120315e3beed830
Ronald Ogando Negron <ronaldogando150@gmail.com>
2020-03-23 23:39:17 -0400

    initial commit

---
+++ src/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'mysecrectkey',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+// Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
+// Routes
+app.use(require('./routes/index.routes'));
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
